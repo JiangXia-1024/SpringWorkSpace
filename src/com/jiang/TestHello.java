74b78e0059a97909162fecfdef4ba0b20974cad8
JIANG\Administrator <j17621607346@163.com>
2021-07-12 21:37:57 +0800

    spring ioc 案例

--- src/com/jiang/TestHello.java
+++ src/com/jiang/TestHello.java
@@ -1,4 +1,17 @@
 package com.jiang;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestHello {
+    @Test
+    public void test(){
+        // 1、读取配置文件实例化一个IOC容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("helloword.xml");
+        // 2、从容器中获取Bean，注意此处完全“面向接口编程，而不是面向实现”
+        HelloService helloService = context.getBean("helloService",HelloService.class);
+        // 3、执行业务逻辑
+        helloService.say();
+    }
 }
